a6dc1deaa510ae5c4af09e16954ca68e6f86ce84
Ibrahim <ihim512@gmail.com>
2023-08-31 13:30:58 +0700

    update after sidang & add second model

--- predict.py
+++ predict.py
@@ -1,36 +1,60 @@
 import cv2
 import numpy as np
 from keras.models import load_model
+from keras.optimizers import Adam
 from keras.utils import img_to_array
 from PIL import Image
 
-file_model = 'model/model_mobileNetV3-91.h5'
-labels = ["Covid-19", "Pneumonia","Normal", "Tuberkolosis"]
+file_model = "model/model_mobileNetV3-91.h5"
+file_model_2 = "model/xray_nonxray.h5"
+labels = ["Covid-19", "Normal", "Tuberkolosis", "Pneumonia"]
+labels_2 = ["nonxray", "xray"]
+
 
 def predict(path):
-    model = load_model(file_model)
+    model_2 = load_model(file_model_2, compile=False)
+    model = load_model(file_model, compile=False)
+    # model.compile(
+    #     loss="categorical_crossentropy",
+    #     optimizer=Adam(learning_rate=0.00001),
+    # )
     data = np.zeros((1, 224, 224, 3))
     img = cv2.imread(path)
-    img = cv2.resize(img,(224,224))
+    img = cv2.resize(img, (224, 224))
     pil_img = Image.fromarray(img)
-    data[0]+=img_to_array(pil_img)
+    data[0] += img_to_array(pil_img)
     # data/=255.0
-    y_pred = model.predict(data)  
+    y_pred = model_2.predict(data)
 
+    dict_result_1 = {}
+    for i in range(2):
+        dict_result_1[y_pred[0][i]] = labels_2[i]
+    penentuan = y_pred[0]
+    penentuan.sort()
+    penentuan = penentuan[::-1]
+    prob = penentuan[:4]
+    prob_result = []
+    class_result = []
+    for i in range(2):
+        prob_result.append((prob[i] * 100).round(2))
+        class_result.append(dict_result_1[prob[i]])
+    print("test masok!" + str(class_result) + str(prob_result) + str(y_pred[0][0]))
+    if class_result[0] == "nonxray" or y_pred[0][0] >= 0.1:
+        class_result = labels_2
+        return class_result, prob_result, y_pred
+
+    y_pred = model.predict(data)
     dict_result = {}
-
-    for i in range(4) :
+    for i in range(4):
         dict_result[y_pred[0][i]] = labels[i]
-            
     res = y_pred[0]
     res.sort()
     res = res[::-1]
-    prob=res[:4]
-    prob_result =[]
-    class_result=[]
-
+    prob = res[:4]
+    prob_result = []
+    class_result = []
     for i in range(4):
-        prob_result.append ((prob[i]*100).round(2))
+        prob_result.append((prob[i] * 100).round(2))
         class_result.append(dict_result[prob[i]])
 
-    return class_result, prob_result
+    return class_result, prob_result, y_pred
--- route.py
+++ route.py
@@ -7,88 +7,115 @@ from werkzeug.utils import secure_filename
 
 from predict import predict
 
-app = Flask(__name__,static_url_path='/assets',
-            static_folder='./public/assets', 
-            template_folder='./public')
-app.config['SECRET_KEY']='secretkey'
-app.config['UPLOAD_FOLDER'] = 'public/assets/temp'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app = Flask(
+    __name__,
+    static_url_path="/assets",
+    static_folder="./public/assets",
+    template_folder="./public",
+)
+app.config["SECRET_KEY"] = "secretkey"
+app.config["UPLOAD_FOLDER"] = "public/assets/temp"
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
+
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         return post()
     else:
-        return render_template('index.html')
+        return render_template("index.html")
         # return 'Welcome to My Classification Lung Disease API!!!'
-@app.route('/api/predict', methods=['POST'])
+
+
+@app.route("/api/predict", methods=["POST"])
 def apiPredict():
     return postPredict()
 
 
 def post():
     # check if the post request has the file part
-    if 'file' not in request.files: 
-        flash('No file part in the request')
+    if "file" not in request.files:
+        flash("No file part in the request")
         return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No file selected for uploading')
+    file = request.files["file"]
+    if file.filename == "":
+        flash("No file selected for uploading")
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(path)
-        print('path: ', path)
+        print("path: ", path)
         pred = predict(path)
-        
+
         file = cv2.imread(path)
         file = cv2.cvtColor(file, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(path,file)
-        
-        probability = {pred[0][i]: pred[1][i] for i in range(0, len(pred[0]))}
-        resp = {
-            'image' : 'temp/'+filename, 
-            'output': pred[0][0],
-            'probability': probability,
+        cv2.imwrite(path, file)
+        resp = {}
+
+        if len(pred[0]) == 2:
+            print("predict 1: ", str(pred[0][0]) + str(len(pred[0])))
+            resp = {
+                "image": "temp/" + filename,
+                "output": pred[0][0],
             }
-        return render_template('index.html', data=resp)
+        else:
+            print("predict 2: ", str(pred[0][0]) + str(len(pred[0])))
+            probability = {pred[0][i]: pred[1][i] for i in range(0, len(pred[0]))}
+            resp = {
+                "image": "temp/" + filename,
+                "output": pred[0][0],
+                "probability": probability,
+            }
+        return render_template("index.html", data=resp)
     else:
-        flash('Allowed file types are png, jpg, jpeg')
+        flash("Allowed file types are png, jpg, jpeg")
         return redirect(request.url)
-    
+
+
 def postPredict():
     # check if the post request has the file part
-    if 'file' not in request.files:        
-        resp = jsonify({'message' : 'No file part in the request'})
+    if "file" not in request.files:
+        resp = jsonify({"message": "No file part in the request"})
         resp.status_code = 400
         return resp
-    file = request.files['file']
-    if file.filename == '':
-        resp = jsonify({'message' : 'No file selected for uploading'})
+    file = request.files["file"]
+    if file.filename == "":
+        resp = jsonify({"message": "No file selected for uploading"})
         resp.status_code = 400
         return resp
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(path)
         pred = predict(path)
-        probability = {pred[0][i]: pred[1][i] for i in range(0, len(pred[0]))}
-        print(probability)
-        resp = jsonify({
-            'message' : 'successfully', 
-            'output': pred[0][0],
-            'probability': probability,
-            })
+        resp = jsonify({})
         resp.status_code = 201
+
+        resp = {}
+
+        if len(pred[0]) == 2:
+            print("predict 1: ", str(pred[0][0]) + str(len(pred[0])))
+            resp = {
+                "image": "temp/" + filename,
+                "output": pred[0][0],
+            }
+        else:
+            print("predict 2: ", str(pred[0][0]) + str(len(pred[0])))
+            probability = {pred[0][i]: pred[1][i] for i in range(0, len(pred[0]))}
+            resp = {
+                "image": "temp/" + filename,
+                "output": pred[0][0],
+                "probability": probability,
+            }
         return resp
     else:
-        resp = jsonify({'message' : 'Allowed file types are png, jpg, jpeg'})
+        resp = jsonify({"message": "Allowed file types are png, jpg, jpeg"})
         resp.status_code = 400
         return resp
